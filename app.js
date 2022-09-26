5ffbf4044e4829e6a4a4c7a49070e9501775ed2a
Daniel Parkhanovich <daniel.parkhanovich@gmail.com>
2022-09-26 20:52:13 +0200

    Added Http request for Midi.

--- app.js
+++ app.js
@@ -1,17 +1,13 @@
 
+// const SERVER_URL = "http://127.0.0.1:8080"
+
 function updateTrackDisplay() {
 
     const inputElement = document.getElementById("input");
     const fileList = inputElement.files;
     const file = fileList[0];
     
-    const url = URL.createObjectURL(file);
-
-    const player = document.getElementById("player");
-    const visualizer = document.getElementById("visualizer");
-
-    player.src = url;
-    visualizer.src = url;
+    replaceTrack(file)
 }
 
 function updateVisualizerType() {
@@ -22,6 +18,36 @@ function updateVisualizerType() {
   const visualizer = document.getElementById("visualizer");
 
   visualizer.type = selectedStr;
+}
+
+function getMIDIRequest() {
+
+    //const query = "MIDI"
+    // const url = SERVER_URL + "/?name=" + query
+    
+    const url = "http://150.254.131.192:8080/inference"
+    console.log("sended to: " + url)
+
+    const req = new XMLHttpRequest();
+    req.open("GET", url, true);
+    req.responseType = "blob";
+    req.onload = (event) => {
+      const blob = req.response;
+      console.log("Received: " + blob)
+      replaceTrack(blob)
+  };
+  
+  req.send();
+}
+
+function replaceTrack(file) {
+  const url = URL.createObjectURL(file);
+
+  const player = document.getElementById("player");
+  const visualizer = document.getElementById("visualizer");
+
+  player.src = url;
+  visualizer.src = url;
 }
 
 function main() {
